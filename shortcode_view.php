675feaec546d055b047f7735f66397aec8d73fe7
mra13 <mra13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-28 05:50:38 +0000

    committing v3.1
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-easy-paypal-payment-or-donation-accept-plugin/trunk@779469 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode_view.php
+++ shortcode_view.php
@@ -1,15 +1,24 @@
-<div id="wp_paypal_button_cont">
+<div class="wp_paypal_button_widget">
+	<form name="_xclick" class="wp_accept_pp_button_form" action="https://www.paypal.com/cgi-bin/webscr" method="post">	
+		<div class="wp_pp_button_selection_section">
+		<select class="wp_paypal_button_options">
+			<?php echo $html_options; ?>
+		</select>
+		</div>
+		
+		<?php 
+		if(!empty($other_amount)){
+			echo '<div class="wp_pp_button_other_amt_section">';
+			echo 'Other Amount: <input type="text" name="other_amount" value="" size="4">';
+			echo '</div>';
+		}
+		?>
 
-	<select id="wp_paypal_button_options">
-		<?php echo $html_options; ?>
-	</select>
+		<div class="wp_pp_button_reference_section">
+		<label for="wp_pp_button_reference"><?php echo $reference; ?></label>
+		<br /><input type="text" name="os0" class="wp_pp_button_reference" />
+		</div>
 
-	<br />
-
-	<label for="customer_email_address"><?php echo $reference; ?></label> <br />
-	<p><input type="text" id="customer_email_address" /></p>
-
-	<form name="_xclick" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 		<input type="hidden" name="cmd" value="_xclick">
 		<input type="hidden" name="business" value="<?php echo $email; ?>">
 		<input type="hidden" name="currency_code" value="<?php echo $currency; ?>">
@@ -17,27 +26,35 @@
 		<input type="hidden" name="amount" value="">
 		<input type="hidden" name="return" value="<?php echo $return; ?>" />
 		<input type="hidden" name="email" value="" />
-	</form>
-	<input type="image" id="buy_now_button" src="<?php echo $payment_button_img_src; ?>" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+		<div class="wp_pp_button_submit_btn">
+			<input type="image" id="buy_now_button" src="<?php echo $payment_button_img_src; ?>" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+		</div>
+	</form>	
 </div>
 
 <style>
-#wp_paypal_button_options{
-	margin-bottom: 15px;
+.wp_pp_button_selection_section, .wp_pp_button_other_amt_section, .wp_pp_button_reference_section{
+margin-bottom: 10px;
+}
+.wp_paypal_button_widget{
+margin: 10px 0;
 }
 </style>
 
 <script type="text/javascript">
-jQuery('#wp_paypal_button_cont #buy_now_button').click(function(e){
-	e.preventDefault();
-	var price = jQuery('#wp_paypal_button_options').val();
-	var name = jQuery('#wp_paypal_button_options :selected').attr('data-product_name');
-	var email = jQuery('#customer_email_address').val();
-	
-	jQuery('input[name=email]').val(email);
-	jQuery('input[name=amount]').val(price);
-	jQuery('input[name=item_name]').val(name);
-
-	jQuery('#wp_paypal_button_cont form').submit();
+jQuery(document).ready(function($) {
+	$('.wp_accept_pp_button_form').submit(function(e){	
+		var form_obj = $(this);
+		var options_name = form_obj.find('.wp_paypal_button_options :selected').attr('data-product_name');
+		form_obj.find('input[name=item_name]').val(options_name);
+		
+		var options_val = form_obj.find('.wp_paypal_button_options').val();
+		var other_amt = form_obj.find('input[name=other_amount]').val();
+		if (!isNaN(other_amt) && other_amt.length > 0){
+			options_val = other_amt;
+		}
+		form_obj.find('input[name=amount]').val(options_val);
+		return;
+	});
 });
 </script>
